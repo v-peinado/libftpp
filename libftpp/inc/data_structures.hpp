b9185e5e9c0f03417f292ee1449f104dec56730d
v-peinado <peinado1985@gmail.com>
2025-09-23 21:08:14 +0200

    ADD: data buffer

--- libftpp/inc/data_structures.hpp
+++ libftpp/inc/data_structures.hpp
@@ -2,5 +2,6 @@
 # define DATA_STRUCTURES_HPP
 
 #include "data_structures/pool.hpp"
+#include "data_structures/data_buffer.hpp"
 
 #endif
---
+++ libftpp/inc/data_structures/data_buffer.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file data_buffer.hpp
+ * @brief Polymorphic container for storing objects in byte format.
+ */
+
+#ifndef DATA_BUFFER_HPP
+# define DATA_BUFFER_HPP
+
+
+
+#endif
